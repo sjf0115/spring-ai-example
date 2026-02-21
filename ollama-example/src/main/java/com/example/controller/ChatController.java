71b6b5fac3e0481b0021449fc3e1fd2c8ee68019
sjf0115 <1203745031@qq.com>
2026-02-22 00:52:50 +0800

    MOD: Ollama Example

--- ollama-example/src/main/java/com/example/controller/ChatController.java
+++ ollama-example/src/main/java/com/example/controller/ChatController.java
@@ -1,12 +1,15 @@
 package com.example.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 /**
  * 功能：ChatController
@@ -17,15 +20,25 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Slf4j
 @RestController
-@RequestMapping("/api/chat")
+@RequestMapping("/api/ollama")
 public class ChatController {
     @Autowired
     private OllamaChatModel ollamaChatModel;
 
-    @GetMapping("/ollama")
+    @GetMapping("/chat")
     public String chat(@RequestParam(value = "message", defaultValue = "你是谁") String message) {
-        String response = this.ollamaChatModel.call(message);
+        // String response = this.ollamaChatModel.call(message);
+        Prompt prompt = new Prompt(new UserMessage(message));
+        String response = ollamaChatModel.call(prompt).getResult().getOutput().getText();
         log.info("response : {}", response);
         return response;
+    }
+
+    @GetMapping(value = "/chat/stream", produces = "text/plain;charset=UTF-8")
+    public Flux<String> chatStream(@RequestParam(value = "message", defaultValue = "你是谁") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        // 使用 stream 方法返回 Flux<ChatResponse>
+        return ollamaChatModel.stream(prompt)
+                .map(response -> response.getResult().getOutput().getText());
     }
 }
