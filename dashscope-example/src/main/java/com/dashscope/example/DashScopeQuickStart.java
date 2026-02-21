771d543f750b0460e3e030c8126ed15bb5b038d8
sjf0115 <1203745031@qq.com>
2026-02-21 09:48:40 +0800

    MOD: 调用大模型API入门示例

---
+++ dashscope-example/src/main/java/com/dashscope/example/DashScopeQuickStart.java
@@ -0,0 +1,57 @@
+package com.dashscope.example;
+
+import com.alibaba.dashscope.aigc.generation.Generation;
+import com.alibaba.dashscope.aigc.generation.GenerationParam;
+import com.alibaba.dashscope.aigc.generation.GenerationResult;
+import com.alibaba.dashscope.common.Message;
+import com.alibaba.dashscope.common.Role;
+import com.alibaba.dashscope.exception.ApiException;
+import com.alibaba.dashscope.exception.InputRequiredException;
+import com.alibaba.dashscope.exception.NoApiKeyException;
+
+import java.util.Arrays;
+
+/**
+ * 功能：调用大模型API入门示例
+ * 作者：@SmartSi
+ * 博客：https://smartsi.blog.csdn.net/
+ * 公众号：大数据生态
+ * 日期：2026/2/21 00:36
+ */
+public class DashScopeQuickStart {
+
+    // 调用大模型API
+    public static GenerationResult callWithMessage() throws ApiException, NoApiKeyException, InputRequiredException {
+        Generation gen = new Generation();
+        Message systemMsg = Message.builder()
+                .role(Role.SYSTEM.getValue())
+                .content("You are a helpful assistant.")
+                .build();
+
+        Message userMsg = Message.builder()
+                .role(Role.USER.getValue())
+                .content("你是谁？")
+                .build();
+
+        GenerationParam param = GenerationParam.builder()
+                // 若没有配置环境变量，请用阿里云百炼API Key将下行替换为：.apiKey("sk-xxx")
+                .apiKey(System.getenv("DASHSCOPE_API_KEY"))
+                // 模型列表：https://help.aliyun.com/model-studio/getting-started/models
+                .model("qwen-plus")
+                .messages(Arrays.asList(systemMsg, userMsg))
+                .resultFormat(GenerationParam.ResultFormat.MESSAGE)
+                .build();
+        return gen.call(param);
+    }
+
+    public static void main(String[] args) {
+        try {
+            GenerationResult result = callWithMessage();
+            System.out.println(result.getOutput().getChoices().get(0).getMessage().getContent());
+        } catch (ApiException | NoApiKeyException | InputRequiredException e) {
+            System.err.println("错误信息："+e.getMessage());
+            System.out.println("请参考文档：https://help.aliyun.com/model-studio/developer-reference/error-code");
+        }
+        System.exit(0);
+    }
+}
